79107cb681f8552d54befa900f9445308ee5faa4
bilsina <bils1n@icloud.com>
2023-06-08 00:23:29 +0300

    add example_5

---
+++ example_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name >> ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+        Console.WriteLine("It's Masha! Finally!");
+}
+else
+{
+        Console.Write("Hello, ");
+        Console.Write(username);
+}
